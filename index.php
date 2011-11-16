6f5f06ad9ebd41419c79eec7fa487812afdef711
Thibaud Rohmer <thibaud.rohmer@gmail.com>
2011-11-16 00:42:54 +0000

    Added comments to the index file

--- index.php
+++ index.php
@@ -1,39 +1,59 @@
 <?php
-/*
-    This file is part of PhotoShow.
+/**
+ * This file implements the index.
+ * 
+ * PHP versions 4 and 5
+ *
+ * LICENSE:
+ * 
+ * This file is part of PhotoShow.
+ *
+ * PhotoShow is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PhotoShow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PhotoShow.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  Website
+ * @package   Photoshow
+ * @author    Thibaud Rohmer <thibaud.rohmer@gmail.com>
+ * @copyright 2011 Thibaud Rohmer
+ * @license   http://www.gnu.org/licenses/
+ * @link      http://github.com/thibaud-rohmer/PhotoShow-v2
+ */
 
-    PhotoShow is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    PhotoShow is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with PhotoShow.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
+/// Start session
 session_start();
 
+/// Autoload classes
 function __autoload($class){
 	require_once(realpath(dirname(__FILE__)."/src/classes/$class.class.php"));
 }
 
+/// Take care of nasty exceptions
 function exception_handler($exception) {
   echo "<div class='exception'>Exception : " , $exception->getMessage(), "</div>\n";
 }
 set_exception_handler('exception_handler');
 
+/// Initialize variables
 try{
 	Settings::init();
 	CurrentUser::init();	
 }catch(Exception $e){
+	
 	// User is not logged. Should we display a form then ?
+
 }
 
+/// Check what to do
 switch (CurrentUser::$action){
 	
 	case "Page":
