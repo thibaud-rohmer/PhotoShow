c51cb596b3138f8e399d516e576749c16a49b184
Thibaud Rohmer <thibaud.rohmer@gmail.com>
2011-12-02 22:36:57 +0000

    Added caching to images accessed by PhotoShow's PHP Proxy

--- src/classes/Provider.php
+++ src/classes/Provider.php
@@ -125,6 +125,10 @@ class Provider
 			$path = $file;
 		}
 
+		$expires = 60*60*24*14;
+		header("Pragma: public");
+		header("Cache-Control: maxage=".$expires);
+		header('Expires: ' . gmdate('D, d M Y H:i:s', time()+$expires) . ' GMT');
 		header('Content-type: image/jpeg');
 		readfile($path);			
 	}
