38f29d692acf1673d4a20a1d3ae1cb6afbb2df7e
saintgermain <saintger@gmail.com>
2015-08-27 00:13:50 +0200

    Streaming for Applie iPhone
    iPhone needs additional code in order to be able to read videos
    Code is taken from here (no license specified, hope that it is not copyright violation) :
    https://mobiforge.com/design-development/content-delivery-mobile-devices

--- src/classes/Provider.php
+++ src/classes/Provider.php
@@ -148,7 +148,58 @@ class Provider
         header("Cache-Control: maxage=".$expires);
         header('Expires: ' . gmdate('D, d M Y H:i:s', time()+$expires) . ' GMT');
         header('Content-type: video/mp4');
-        readfile($path);
+        $fp = @fopen($path, 'rb');
+
+        $size   = filesize($path); // File size
+        $length = $size;           // Content length
+        $start  = 0;               // Start byte
+        $end    = $size - 1;       // End byte
+
+        header("Accept-Ranges: 0-$length");
+        if (isset($_SERVER['HTTP_RANGE'])) {
+
+            $c_start = $start;
+            $c_end   = $end;
+
+            list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+            if (strpos($range, ',') !== false) {
+                header('HTTP/1.1 416 Requested Range Not Satisfiable');
+                header("Content-Range: bytes $start-$end/$size");
+                exit;
+            }
+            if ($range == '-') {
+                $c_start = $size - substr($range, 1);
+            }else{
+                $range  = explode('-', $range);
+                $c_start = $range[0];
+                $c_end   = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
+            }
+            $c_end = ($c_end > $end) ? $end : $c_end;
+            if ($c_start > $c_end || $c_start > $size - 1 || $c_end >= $size) {
+                header('HTTP/1.1 416 Requested Range Not Satisfiable');
+                header("Content-Range: bytes $start-$end/$size");
+                exit;
+            }
+            $start  = $c_start;
+            $end    = $c_end;
+            $length = $end - $start + 1;
+            fseek($fp, $start);
+            header('HTTP/1.1 206 Partial Content');
+	    }
+        header("Content-Range: bytes $start-$end/$size");
+        header("Content-Length: ".$length);
+
+        $buffer = 1024 * 8;
+	    while(!feof($fp) && ($p = ftell($fp)) <= $end) {
+
+            if ($p + $buffer > $end) {
+                $buffer = $end - $p + 1;
+            }
+            set_time_limit(0);
+            echo fread($fp, $buffer);
+            flush();
+	    }
+        fclose($fp);
     }
 
     public static function thumb($file)
@@ -337,8 +388,61 @@ class Provider
                     readfile($path);
                 }
             }else{
+                $fp = @fopen($path, 'rb');
+
+                $size   = filesize($path); // File size
+                $length = $size;           // Content length
+                $start  = 0;               // Start byte
+                $end    = $size - 1;       // End byte
+
                 header('Content-type: video/mp4');
-                readfile($path);
+                header("Accept-Ranges: 0-$length");
+                if (isset($_SERVER['HTTP_RANGE'])) {
+
+                    $c_start = $start;
+                    $c_end   = $end;
+
+                    list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+                    if (strpos($range, ',') !== false) {
+                        header('HTTP/1.1 416 Requested Range Not Satisfiable');
+                        header("Content-Range: bytes $start-$end/$size");
+                        exit;
+                    }
+                    if ($range == '-') {
+                        $c_start = $size - substr($range, 1);
+                    }else{
+                        $range  = explode('-', $range);
+                        $c_start = $range[0];
+                        $c_end   = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
+                    }
+                    $c_end = ($c_end > $end) ? $end : $c_end;
+                    if ($c_start > $c_end || $c_start > $size - 1 || $c_end >= $size) {
+                        header('HTTP/1.1 416 Requested Range Not Satisfiable');
+                        header("Content-Range: bytes $start-$end/$size");
+                        exit;
+                    }
+                    $start  = $c_start;
+                    $end    = $c_end;
+                    $length = $end - $start + 1;
+                    fseek($fp, $start);
+                    header('HTTP/1.1 206 Partial Content');
+                }
+                header("Content-Range: bytes $start-$end/$size");
+                header("Content-Length: ".$length);
+
+
+                $buffer = 1024 * 8;
+                while(!feof($fp) && ($p = ftell($fp)) <= $end) {
+
+                    if ($p + $buffer > $end) {
+                        $buffer = $end - $p + 1;
+                    }
+                    set_time_limit(0);
+                    echo fread($fp, $buffer);
+                    flush();
+                }
+
+                fclose($fp);
             }
         }
     }
